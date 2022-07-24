2438b49d0169582684377c7b2cfe72af5f0c5541
Rachelle DeMan <demanr@mcmaster.ca>
2022-07-23 23:56:26 -0400

    Working navbar

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import './App.css';
 import Navbar from "./components/Navbar"
 import Showcase from "./components/Showcase"
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Profile from "./components/Profile"
+import CompletedProjects from './components/CompletedProjects'
+import FutureProjects from './components/FutureProjects';
+
+
 /* add element={} with components needed for route path to fix if broken*/
 function App() {
   return (
     <Router>
       <Navbar />
-      <Routes> 
-        <Route path="/"/>
-        <Route path="/profile"/>
-        <Route path="/finishedProjects"/>
-        <Route path="/futureProjects"/>
-      </Routes>
+      <div className="container">
+        <Routes> 
+          <Route path="/" element={<Showcase />} />
+          <Route path="/Profile" element={<Profile />}/>
+          <Route path="/CompletedProjects" element={<CompletedProjects />}/>
+          <Route path="/FutureProjects" element={<FutureProjects />}/>
+        </Routes>
+      </div>
     </Router>
   );
 }
