d0106edf824071d1a35aebacce5ce1642dfcab8c
Rachelle DeMan <demanr@mcmaster.ca>
2022-07-28 00:24:27 -0400

    Added all projects as of August 28

--- src/projectData.js
+++ src/projectData.js
@@ -7,10 +7,21 @@ export default {
                 name: "Hornet Plush",
                 imageUrl: "hornetPlush",
                 totalImages: 3,
-                description: "TBA",
+                description: "Plush of Hornet from Hollow Knight",
                 dateStarted: "2022-06-07",
                 dateEnded: "2022-07-19",
                 hoursSpent: 12,
+                fullDescription: `This project was a huge challenge for me to tackle, or it seemed so when I started it. It would be my first time 
+                following a more complex pattern, making a proper plush and using (correct) sewing. Furthermore, the fact my instructions were translated spanish 
+                introduced some unique difficulties, such as incomprehensible instructions, or contradictory statements. I only realized 70% into the project that I 
+                used a yarn type far larger than the example, and as such my plush is a bit chunkier, being forearm length instead of hand sized. The eyes were the most 
+                frustrating part, as my one horn was a bit off due to translation misunderstanding, and so the head stiches were uneven on the one side. Furthermore, I 
+                wanted to take the plush to the next level, and so I had to make the moving gravity defying yarn strand for Hornet's sewing needle. This posed many issues. 
+                As i didn;t have any pipe cleaner, I used two mask wires to create the thread. That was, until it startd falling apart and bending the needle. After that I 
+                scavenged my house for pipe cleaners and found a singular while pipe cleaner. With difficulty, I threaded it into the needle, and my plush was complete.`,
+                patternDescription: `I used a spanish instruction from youtuber CatalinArte Tejidos to make this project. I decided to add 
+                some moveable thread to the needle, which was made by doing single crochets around a pipe cleaner. There were some other small adjustments as well, 
+                such as making the dress longer due to using a larger thread (to have a better leg-body ratio) and sewing a line of single crochet into the neck`
                 
             },
             {
@@ -18,34 +29,132 @@ export default {
                 name: "Whale Plush",
                 imageUrl: "whalePlush",
                 totalImages: 5,
-                description: "TBA",
+                description: "Mid blue whale plush",
                 dateStarted: "2022-07-14",
                 dateEnded: "2022-07-22",
                 hoursSpent: 6,
-                  
+                fullDescription: `This whale plush is a gift to Iroise for her birthday. I started it on the train home, and completed it not long after. I contemplated 
+                restarting or scrapping this project entirely due to difficulties with having a different yarn size than the pattern (leading to odd proportions), and due to my 
+                cleaner colour change strategy making a very odd tightness. I didn't want to frog, and when asking my friend Arian his opinion he said to finish it. I am 
+                quite glad I did, as it turned out well in the end. It looks a bit strange when sat directly on a table, as its blushy cheeks and stomach are nearly fully hidden (likely 
+                due to my colour changing strategy), but when hung it looks fabulous.`,
+                patternDescription: `I used a pattern for this whale, which can be found through this link. https://www.hookedbyrobin.com/blog/amigurumi-crochet-whale-pattern .
+                I made a few adjustments to this pattern- I used invisible decreases throughout instead of for only certain parts, and I used an invisble colour 
+                change method to make a cleaner seam. This backfired slightly, as it created a small ridge between the colours, but by completion it was nearly invisible. 
+                Another addition I made was the spout on top. Using single crochets around a mask wire (re-used from my first attempt at the hornet plush thread) I sewed in the 
+                remaining thread from closing the bottom to make it hangable.`
             },
             {
-                id: 1,
-                name: "Hornet Plush",
-                imageUrl: "hornetPlush",
+                id: 3,
+                name: "Flower",
+                imageUrl: "flower",
+                totalImages: 4,
+                description: "Flower with grey stem",
+                dateStarted: "2022-05-02",
+                dateEnded: "2022-05-02",
+                hoursSpent: 2,
+                fullDescription: `This was created as a mother's day gift. Due to my mother's birthday being a few days befpre mothers day, in 
+                the past my brother and I have struggled to find an adequate gift. Recently, my brother had covid, and despite the mandatory isolation time 
+                and a negative test he was kept home to stay on the safe side. Due to this, I was in charge of both mother's day and birthday gift. When I asked my 
+                brother for ideas he told me to "buy flowers" as that is what he normally does, but I thought I might be able to do better. That lead to the creation 
+                of this flower. I struggled due to my inexperience with sewing, this being the first project that heavily relied on it to tie the petals together, but I 
+                handled it moderately well. The stem was a personal addition, due to the lone flower feeling a bit lacklustre. I created the flower sans stem again for 
+                my grandmother on approximately May 11th, but due to lack of images and distinguishing photos from this project, it will not get its own profile.`,
+                patternDescription: `To make this flower, start by chaining 60 single loops. Do these slightly loosely, as you will be crocheting into the chains. Chaining by any similar multiple of 4 works as well. After chaining, 
+                you can add extra loops in case some are missed. Then, skipping one, do eight double threads in one hole. After those are complete, 
+                skip a stitch and do a slip knot in the second stitch. Then, skipping a stitch into the second, continue the pattern of eight double threads. 
+                The result is a string of petals, which are then sewn circularly to make a flower. I created a inside portion from a magic loop I 
+                sewed in, and a stem from chaining and then slit stitching into every other stitch back down, creating a leaf the same way you would 
+                create a petal.`
+            },
+            {
+                id: 4,
+                name: "Rat Plush",
+                imageUrl: "ratPlush",
+                totalImages: 1,
+                description: "Cone shaped rat plush",
+                dateStarted: "2021-01-02",
+                dateEnded: "2022-01-06",
+                hoursSpent: 6,
+                fullDescription: `This project was a gift for my friend Iroise for Christmas. It was my first attempt at a plush, and I did
+                not use a pattern or follow a video, resulting in its cone shape. Despite its imperfections, I am very happy with how it turned out. 
+                It looks "derpy" in the cute way I was aiming for, knowing a smooth plush was unlikely to result from my attempts. At this time, I had no 
+                sewing needles, and due to this heavily avoided any sewing. The tail was a line of single crochets from creating the circular bottom, leaving 
+                the ears as the only part that needed to be sewn. I wrapped my yarn several times around the front of my crochet hook, trapping it under the notch 
+                to drag it through and out of the plush. It was very tedious and slow going, but the method worked eventually. I am very happy to own proper 
+                needles now, as it makes this process so much more enjoyable. The rat is approximately palm sized.` ,
+                patternDescription: `This is made using solely medium (4) yarn. It begins by making a standard circle,
+                which is done by first creating a standard magic ring of any length, and do double threads
+                through the ring (it is recommended do 6-8, my example had 8). After the first ring is 
+                complete do a single loop through the top of the first stitch in the previous row, and do a 
+                single loop through the current strand, and crochet through the tops of the previous row. At
+                a decided interval do two double threads into one whole to create a circle. To have the sphere
+                be flatter do a row with only one double thread in each previous top. Afrer the sphere has reached
+                the desired size begin to do double threads into the previous row but at a decided interval (once
+                every 3 is a good starting point) do a double thread through two holes at once (it will have the 
+                same amount of string as would appear in a triple thread). For a sphere you make the interval of
+                decrease the same as that of increase, but for the rat I needed a cone shape and so that step does
+                not apply. For the ears I did 8 single loops and double threaded into a hole 2 down, then did two single
+                loops and threaded the closes hole until I had a "U" shape. With the final string I threaded the middle
+                parts together.`
+            },
+            {
+                id: 5,
+                name: "Navy Scarf",
+                imageUrl: "navyScarf",
                 totalImages: 3,
-                description: "TBA",
-                dateStarted: "2022-06-07",
-                dateEnded: "2022-07-19",
-                hoursSpent: 12,
-                
+                description: "Long navy scarf",
+                dateStarted: "2021-12-28",
+                dateEnded: "2022-01-05",
+                hoursSpent: 5,
+                fullDescription: `Standing at around 4 feet 10 inches, this scarf ended up being a bit bigger than I thought. It was 
+                made as a Christmas gift for my boyfriend, Wally. There is nothing inherently special about it, but it was a nice Christmas break 
+                project. It took equally as long (if not longer) than my simple scarf due to being almsot a foot longer.`,
+                patternDescription: `As with all my early projects, this is made using medium (4) yarn. This pattern is the same as the grey regular scarf with some exceptions. Firstly, it is less wide,
+                having an original length of 22 single loops, and is longer, with a final length of around 4 feet 10 inches.`
             },
             {
-                id: 2,
-                name: "Whale Plush",
-                imageUrl: "whalePlush",
-                totalImages: 5,
-                description: "TBA",
-                dateStarted: "2022-07-14",
-                dateEnded: "2022-07-22",
-                hoursSpent: 6,
-                  
+                id: 6,
+                name: "Dishcloth",
+                imageUrl: "dishcloth",
+                totalImages: 2,
+                description: "White with spots of blue and yellow dishcloth",
+                dateStarted: "2021-12-27",
+                dateEnded: "2021-12-27",
+                hoursSpent: 2,
+                fullDescription: `This dishcloth was made while visiting my father and having a long family Christmas discussion. It was made as a gift to my mother. 
+                I enjoy fidgeting, and as such having a project to work on while in a group discussion was a lifesaver. Furthermore, when the subject was dull, I had 
+                something to put my attention on besides the wall or my phone (which would be rude!). This was my first time making a border, and while the creation itself 
+                is a bit lopsided, I find the border satisfactory.`,
+                patternDescription: `I am unsure the exact yarn used for this, but it was likely medium. The dishcloth is a simple pattern, similar to a scarf in that it begins with a row of single loops (recommended length of
+                    20-28 stitches) followed by triple loops and single loops respectively. For more detail read simple scarf pattern.
+                    After creating a square-like shape, you can begin creating a border. This is entirely optional. To do this you begin by doing a triple stitch 
+                    into the corner piece you left off at, and then counter-clockwise do single threads into the open holes left on the edges. 
+                    Every time a corner is reached you will do three single threads to create a corner. After you reach the beginning stitch of the border 
+                    you will tie off the yarn by making a loop and pulling the end through. This can be done through the first border piece or by where the final stitch 
+                    was made.`
+            },
+            {
+                id: 7,
+                name: "Grey Scarf",
+                imageUrl: "greyScarf",
+                totalImages: 2,
+                description: "Medium length blue grey scarf",
+                dateStarted: "2021-09-23",
+                dateEnded: "2021-12-22",
+                hoursSpent: 5,
+                fullDescription: `This scarf was made as a gift to my mother. It was a very slow going process, 
+                spanning over two months due to lack of time alone, and motivation. Furthermore, this was my first 
+                full length project after returning to crochet, and as such I faced some beginner struggles. Whether it be 
+                due to distraction or otherwise, quite a lot of frogging was done. In the end, I am very satisfied with the result. 
+                In the past, I wrote it took around half a week of concentrated effort.
+                `,
+                patternDescription: `A simple pattern, make a row of around 20/24 single loop stitches depending on prefered scarf width,
+                single loop twice more are triple thread into the loop two down from the current position. From then on repeat
+                triple threads each stitch until end, with two single loops, flip (always R to L), and proceed with triple threads
+                starting from nearest loop. I did this scarf with a pale blue, it ended up around 4 feet long.`
             }
+            
         ]
     }
 }
