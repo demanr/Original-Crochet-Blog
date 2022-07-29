365c208e0e1ac78ffa5666bc99aa3137812df4c9
Rachelle DeMan <demanr@mcmaster.ca>
2022-07-29 13:13:30 -0400

    Created project template for projects page

--- src/components/ProjectTemplate.js
+++ src/components/ProjectTemplate.js
@@ -1,16 +1,43 @@
 import React from "react"
 import ProjectData from "../projectData.js"
 import {
-    CardContainer,
-    CardImage,
-    CardTitle,
-    CardDescription,
-} from "../styles/Card.style.js"
+    Container,
+    ImageContainer,
+    InfoContainer,
+    Image,
+    Subtitle,
+    Title,
+} from "../styles/ProjectTemplate.style.js"
 
-export default function Card(props) {
+export default function Project(props) {
     const [currentImage, setCurrentImage] = React.useState(1)
+    
+    function changeImage() {
+        setCurrentImage(prev => {
+            var newIndex = 1
+            if (prev != props.item.totalImages){
+                newIndex = prev + 1
+            }
+            return newIndex
+        })
 
+    }
     return (
-        <div></div>
+        <Container >
+            <Title>{props.item.name}</Title>
+            <ImageContainer >
+                <Image src={`/images/${props.item.imageUrl}${currentImage}.jpg`} onClick={changeImage}/>
+            </ImageContainer>
+            
+            <InfoContainer>
+            <Subtitle>Description</Subtitle>
+                {props.item.fullDescription}
+            <Subtitle>Pattern</Subtitle>
+            {props.item.patternDescription}
+            <Subtitle>Completion Dates</Subtitle>
+            This {(props.item.name).toLowerCase()} was started on {props.item.dateStarted} and completed on {props.item.dateEnded}. 
+            This took a total of {props.item.hoursSpent} hours to complete.
+            </InfoContainer>
+        </Container>
         )
 }
