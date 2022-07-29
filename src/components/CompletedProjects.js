bee6a51ddec747e0798b374c80327047241daaa6
Rachelle DeMan <demanr@mcmaster.ca>
2022-07-29 13:12:24 -0400

    Added projects page

--- src/components/CompletedProjects.js
+++ src/components/CompletedProjects.js
@@ -1,14 +1,22 @@
 import React from "react"
 import ProjectData from "../projectData.js"
+import Project from "./ProjectTemplate"
+import {
+    Container,
+} from "../styles/CompletedProjects.style"
 
 export default function CompletedProjects() {
+    const projects = ProjectData.data.completedProjects.map(project => {
+        return (
+            <Project 
+            key={project.id}
+            item={project}
+            />
+    )})
+
     return (
-        <div className="CompletedProjects">
-            <h1>compl projs</h1>
-            <div>
-                <img />
-                <p>custom card(s)</p>
-            </div>
-        </div>
+        <Container>
+            {projects}
+        </Container>
         )
 }
---
+++ src/styles/ProjectTemplate.style.js
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom"
+
+export const Container = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column; 
+    flex-flow: row wrap;
+    justify-content: center;
+    align-items: center;
+    
+`
+
+export const ImageContainer = styled.div`
+    width: 30%;
+    @media (max-width: 1350px) {
+        width: 70%;
+        display: block;
+    }
+`
+
+export const Image = styled.img`
+    width: 100%;
+    height: auto;
+    border-radius: 10px;
+    margin-top: 20px;
+`
+
+export const InfoContainer = styled.div`
+    width: 40%;
+    padding-left: 10%;
+    margin-top: -40px;
+    @media (max-width: 1350px) {
+        width: 70%;
+        padding-left: 0;
+        margin-top: 10px;
+    }
+    
+`
+export const Title = styled.h1`
+    width: 100%;
+    font-size: 36px;
+`
+
+export const Subtitle = styled.h1`
+    width: 100%;
+    font-size: 24px;
+    text-align: left;
+`
